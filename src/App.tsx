bf2e1fb950c1417c2460c900e49c0b30f8cbcfc4
androidblog <androidblog@yandex.ru>
2023-02-15 12:29:48 +0400

    create list of cities with countries to filter the location

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { nanoid } from 'nanoid'
 import './App.css'
 import data from '../node_modules/all-countries-and-cities-json/countries.min.json';
 
-type CitiesWithCountry = {
+type CityWithCountry = {
   city: string
   country: string,
 }
 
 function App() {
   const [searchText, setSearchText] = useState('');
-  const citiesWithCountry: CitiesWithCountry[] = Object.entries(data).map(([country, cities]) =>
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+
+  const citiesWithCountry: CityWithCountry[] = Object.entries(data).map(([country, cities]) =>
     cities.map(city => {
       return {
         country: country,
@@ -19,37 +22,61 @@ function App() {
     })
   ).flat();
 
-  const searchResults: CitiesWithCountry[] = useMemo(() => {
+  const searchResults: CityWithCountry[] = useMemo(() => {
     if (!searchText)
-      return [] as CitiesWithCountry[];
+      return [] as CityWithCountry[];
 
     const cities = citiesWithCountry.filter((el) =>
       el.city.toLowerCase().includes(searchText.toLowerCase())
-    ).slice(0, 10);
+    ).sort().slice(0, 5);
+    if (cities.length === 0) {
+      setCity(searchText)
+    } else {
+      setCity('')
+    }
     //Filter duplicates
-    return cities.filter((tag,index, array) => array.findIndex(t=> t.city === tag.city && t.country === tag.country) === index)
+    return cities.filter((tag, index, array) => array.findIndex(t => t.city === tag.city && t.country === tag.country) === index)
   }, [searchText])
 
   const handleSeach = (e: ChangeEvent<HTMLInputElement>) => {
-    // if (e.target.value)
     setSearchText(e.target.value);
+  }
+
+  const handleSelect = (cityWithCountry: CityWithCountry) => {
+    console.log('ggggg')
+    setCity(cityWithCountry.city)
+  }
+
+  const handleCountryEnter = () => {
+
   }
 
   return (
     <div className="App">
       <div className='modal-container'>
-        <input type='text' onChange={handleSeach}></input>
-      </div>
-      <div>
-        <ul>
-          {
-            searchResults.map((el) => {
-              return <li key={nanoid()}>{el.country}, {el.city}</li>
-            })
-          }
-        </ul>
+        <form>
+          <label htmlFor='city-input'> City
+            <input type='text' id='city-input' list='cities-list' onChange={handleSeach}></input>
+          </label>
+          <label > Country
+            <input type='text'  value={country} disabled={city === ''} ></input>
+          </label>
+
+          <datalist onClick={e => handleSelect(searchResults[0])}
+            id='cities-list'>
+            {
+              searchResults.map((el) => {
+                return <option
+                  value={el.country + ', ' + el.city}
+                  key={nanoid()}
+                ></option>
+              })
+            }
+          </datalist>
+        </form >
       </div>
     </div>
+
   )
 }
 
